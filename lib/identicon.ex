83a6bfd32d5d88b65f11d4802a6a9e5bfd8021f2
alan-maldonado <alan.maldonado@wizeline.com>
2017-02-28 22:52:30 -0600

    prepare to build pixel map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,6 +4,20 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
